a9f3931b9f518af68b114d387dba5db0f7b05aac
Herenot <franciscojavieraquinopiosa@hotmail.com>
2023-07-09 13:42:56 +0200

    upload and drag&drop component created

--- imageUploader/src/app/app.module.ts
+++ imageUploader/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploadImageComponent } from './upload-image/upload-image.component';
+import { DragDropAreaComponent } from './drag-drop-area/drag-drop-area.component';
+import { LoadingModalComponent } from './loading-modal/loading-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadImageComponent,
+    DragDropAreaComponent,
+    LoadingModalComponent
   ],
   imports: [
     BrowserModule,
---
+++ imageUploader/src/app/drag-drop-area/drag-drop-area.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drag-drop-area',
+  templateUrl: './drag-drop-area.component.html',
+  styleUrls: ['./drag-drop-area.component.scss']
+})
+export class DragDropAreaComponent {
+
+}
